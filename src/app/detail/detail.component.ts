cd9ef6e940a34ce4c07106df65465c031dfcad8f
qwekelly <quanwei_1998@163.com>
2022-07-31 14:29:17 +0800

    feat: 引入marked、highlight渲染markdown文件

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { marked } from 'marked';
+import hljs  from 'highlight.js';
+
+import { LoadingService } from 'app/services/loading.service';
+import { BaseService } from 'app/services/base.service';
 
 @Component({
   selector: 'app-detail',
@@ -7,25 +12,40 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./detail.component.scss']
 })
 export class DetailComponent implements OnInit {
+  @ViewChild('content') content!: ElementRef;
 
   fileName = '';
 
   constructor(
     private router: ActivatedRoute,
+    private baseService: BaseService,
+    private loadingService: LoadingService,
   ) {
     this.router.queryParams.subscribe(query => {
       this.fileName = query['fileName']
+      this.loadingService.start()
       this.findAssetFileByName()
     })
   }
 
   ngOnInit() {}
 
-
   findAssetFileByName() {
     if (!this.fileName) {
       return
     }
-    console.log(this.fileName)
+    this.baseService.getMarkDownFile(this.fileName).subscribe((data) => {
+      Promise.resolve().then(() => {
+        this.content.nativeElement.innerHTML = marked.parse(data, {
+          breaks: true,
+          smartLists: true,
+          smartypants: true,
+          highlight: (code, lang, callback) => {
+            return hljs.highlightAuto(code).value;
+          }
+        });
+        this.loadingService.stop()
+      })
+    })
   }
 }
